5322969f6301161107618f7081d0fa77bb0a6dea
DrThuner <72319460+DrThuner@users.noreply.github.com>
2021-06-07 19:48:31 +0200

    abc

---
+++ cmake-build-debug/CMakeFiles/testcpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testcpp.dir/main.cpp.obj"
+  "libtestcpp.dll.a"
+  "testcpp.exe"
+  "testcpp.exe.manifest"
+  "testcpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testcpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void szyfr(string napis, int ile)
+{
+    char x;
+    int a,b,c,d,t;
+
+    for(int i=0; i<napis.length(); i++)
+    {
+        t=napis[i] + ile;
+
+        if(napis[i]>=65 && napis[i]<=90)
+        {
+            if(t>90)
+            {
+                a=t-90;
+                x[i]=64+a;
+            }
+            else if(t<65)
+            {
+                b=65-t;
+                x[i]=91-b;
+            }
+            else
+                x[i]=t;
+        }
+
+        else if(napis[i]>=97 && napis[i]<=122)
+        {
+            if(t>122)
+            {
+                c=t-122;
+                x[i]=96+c;
+            }
+            else if(t<97)
+            {
+                d=97-t;
+                x[i]=123-d;
+            }
+            else
+                x[i]=t;
+        }
+
+        else
+            x[i]=t;
+
+        cout<<x[i];
+    }
+    cout<<endl;
+}
+odszyfr(string napis)
+{
+cout<<napis;
+
+cout<<endl;
+}
+
+int main()
+{
+    string napis,x;
+    int ile;
+    cout<<"Wpisz napis: ";
+    cin>>napis;
+    cout<<endl;
+    cout<<"O ile szyfrowac?: ";
+    cin>>ile;
+    cout<<endl;
+
+    cout<<"szyfrowanie: ";
+    szyfr(napis,ile);
+
+    cout<<"odszyfrowanie: ";
+    odszyfr(napis);
+
+    system("pause");
+    return 0;
+}
